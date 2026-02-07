1d4c5ae6af37c1194d65a4a195add053744a0022
오영민 <ooa0517@gmail.com>
2026-02-07 04:43:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7397291841-37481",
+  "appId": "1:578553179175:web:a0f32f0f19675cbb09b202",
+  "apiKey": "AIzaSyBG9g4T0WiOimSyhozOkrP-NC5M8A3K-es",
+  "authDomain": "studio-7397291841-37481.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "578553179175"
+};
